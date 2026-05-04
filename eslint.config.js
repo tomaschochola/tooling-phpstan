153e39f494476a6fd90b069a472ec6f37524ac69
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-05-04 19:34:46 +0200

    Update: eslint.config.js, prettier.config.js, templates/php_85_library.neon, templates/php_85_project.neon

--- eslint.config.js
+++ eslint.config.js
@@ -10,18 +10,10 @@
  * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
  */
 
-import { Eslint } from '@tomaschochola/tooling-eslint';
-import globals from 'globals';
+import { ESLint } from '@tomaschochola/tooling-eslint';
 
 // eslint-disable-next-line no-restricted-exports
-export default new Eslint()
-  .globals({
-    ...globals.node,
-    ...globals.es2024,
-  })
-  .ignores()
-  .ignores(['node_modules', 'vendor', '.phpunit.cache', '.phpunit.coverage', '.phpunit.result.cache', '.php-cs-fixer.cache'])
-  .recommended()
-  .stylistic()
-  .sonarjs()
-  .build();
+export default new ESLint()
+  .presetNode({ presetDefaultsOptions: { configNodeModulesIgnores: false } })
+  .configIgnores(['node_modules', 'vendor', '.phpunit.cache', '.phpunit.coverage', '.phpunit.result.cache', '.php-cs-fixer.cache'])
+  .buildConfig();
